234742ee59640b0fea53870f4046535f5df1ebec
fersines <fsinesc@gmail.com>
2021-03-18 14:42:35 +0100

    Diferentes Headers según el role

--- FRONT/src/components/PrivateHeader.js
+++ FRONT/src/components/PrivateHeader.js
@@ -2,8 +2,19 @@ import { Link } from "react-router-dom";
 import useAuth from "../shared/hooks/useAuth";
 
 export default function PrivateHeader() {
-  const { signOut } = useAuth();
+  const { signOut, userData } = useAuth();
 
+  if (userData.role === "admin") {
+    return (
+      <header>
+        <Link to="/posts">Buscar Posts</Link>
+        <Link to="/comments">Buscar Comentarios</Link>
+        <Link to="/users">Buscar Usuarios</Link>
+        <button onClick={signOut}>Bye!</button>
+      </header>
+    );
+  } else {
+  }
   return (
     <header>
       <Link to="/posts">Nuevo Link</Link>
--- FRONT/src/pages/PrivateHome.js
+++ FRONT/src/pages/PrivateHome.js
@@ -1,3 +1,11 @@
+import useAuth from "../shared/hooks/useAuth";
+
 export default function PrivateHome() {
-  return <h1>Esta es la Home de un usuario logado</h1>;
+  const { userData } = useAuth();
+  return (
+    <section>
+      <h1>Esta es la Home de un usuario logado</h1>
+      <>{userData.role === "admin" ? "Otra Movida sólo para el Admin" : null}</>
+    </section>
+  );
 }
