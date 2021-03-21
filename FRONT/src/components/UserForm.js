00931542f41fec2cad1b70fe19beec10f365f4fb
fersines <fsinesc@gmail.com>
2021-03-21 19:13:00 +0100

    Probando perfil de usuario

--- FRONT/src/components/UserForm.js
+++ FRONT/src/components/UserForm.js
@@ -1,7 +1,27 @@
+import { useEffect } from "react";
 import useAuth from "../shared/hooks/useAuth";
 
-export default function UserForm(props) {
+const apiUrl = "https://localhost:3000";
+
+export default function UserForm() {
   const { userData } = useAuth();
 
-  return userData.role;
+  const token = localStorage.getItem("token");
+
+  const headers = new Headers();
+  headers.append("Content-Type", "application/json");
+  headers.append("Authorization", token);
+
+  useEffect(() => {
+    console.log(userData);
+    fetch(`${apiUrl}/users/${userData.id}`, { method: "GET", headers: headers })
+      .then((response) => {
+        return response.json();
+      })
+      .then((profile) => {
+        console.log(profile);
+      });
+  }, []);
+
+  return <h1>Aquí deberían salir los datos del perfil del usuario</h1>;
 }
