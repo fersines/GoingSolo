f7d97d82214ef04ec22c15c7abc7b1b5c482ad46
fersines <fsinesc@gmail.com>
2021-03-22 18:47:11 +0100

    Ñapa, uso ListPosts para MasVotados

--- FRONT/src/components/MasVotados.js
+++ FRONT/src/components/MasVotados.js
@@ -1,3 +1,10 @@
+import ListPosts from "./ListPosts";
+
 export default function MasVotados() {
-  return <h1>Aquí saldrían los Posts ordenados por los más votados</h1>;
+  return (
+    <section>
+      <h1>Aquí saldrían los Posts ordenados por los más votados</h1>
+      <ListPosts></ListPosts>
+    </section>
+  );
 }
