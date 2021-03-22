62a8961ab26f761e5492882be38d2aca7030c49a
fersines <fsinesc@gmail.com>
2021-03-22 11:29:14 +0100

    MisLinks (con error supongo de CORS)

--- FRONT/src/components/MisLinks.js
+++ FRONT/src/components/MisLinks.js
@@ -1,3 +1,32 @@
+import { useEffect, useState } from "react";
+import useAuth from "../shared/hooks/useAuth";
+
+const apiUrl = "https://localhost:3000";
+
 export default function MisLinks() {
+  const { userData } = useAuth();
+  const [posts, setposts] = useState(null);
+
+  const token = localStorage.getItem("token");
+
+  const headers = new Headers();
+  headers.append("Content-Type", "application/json");
+  headers.append("Authorization", token);
+
+  useEffect(() => {
+    console.log(userData);
+    fetch(`${apiUrl}/users/${userData.id}/posts`, {
+      method: "GET",
+      headers: headers,
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((profile) => {
+        console.log(profile);
+        setposts(profile.data);
+      });
+  }, []);
+
   return <h1>Aquí saldría el listado con los links del usuario</h1>;
 }
