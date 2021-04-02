61a14603840032a94b20009e829b58997d79291e
fersines <fsinesc@gmail.com>
2021-04-02 11:12:54 +0200

    LinkDetails edit y delete sólo admin o propietario

--- FRONT/src/components/LinkDetails.js
+++ FRONT/src/components/LinkDetails.js
@@ -1,10 +1,13 @@
 import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
+import useAuth from "../shared/hooks/useAuth";
 import DeleteLink from "./DeleteLink";
+import LoveLink from "./LoveLink";
 
 const apiUrl = "http://localhost:3000";
 
 export default function LinkDetails() {
+  const { userData, tokenObject } = useAuth();
   const { id } = useParams();
   const [post, setpost] = useState([]);
 
@@ -25,28 +28,52 @@ export default function LinkDetails() {
       });
   }, []);
 
+  console.log(userData);
+  console.log(post);
+
   const stringDate = new Date(post.date);
   const miDate = stringDate.toString();
 
-  return (
-    <section>
-      <h1>Detalles del Link con id: {id}</h1>
-      <h3>Link</h3>
-      <p>{post.link}</p>
-      <h3>Lo publicaste:</h3>
-      <p>{miDate}</p>
-      <h3>Título</h3>
-      <p>{post.title}</p>
-      <h3>Story</h3>
-      <p>{post.story}</p>
-      <div>
-        <button>
-          <Link to="/editlink">Edita el Link</Link>
-        </button>
-        <button>
-          <DeleteLink></DeleteLink>
-        </button>
-      </div>
-    </section>
-  );
+  if (userData.role === "admin" || userData.id === post.post_user_id) {
+    return (
+      <section>
+        <h1>Detalles del Link con id: {id}</h1>
+        <h3>Link</h3>
+        <p>{post.link}</p>
+        <h3>Fue publicado:</h3>
+        <p>{miDate}</p>
+        <h3>Título</h3>
+        <p>{post.title}</p>
+        <h3>Story</h3>
+        <p>{post.story}</p>
+        <div>
+          <button>
+            <Link to="/editlink">Edita el Link</Link>
+          </button>
+          <button>
+            <DeleteLink></DeleteLink>
+          </button>
+        </div>
+      </section>
+    );
+  } else {
+    return (
+      <section>
+        <h1>Detalles del Link con id: {id}</h1>
+        <h3>Link</h3>
+        <p>{post.link}</p>
+        <h3>Fue publicado:</h3>
+        <p>{miDate}</p>
+        <h3>Título</h3>
+        <p>{post.title}</p>
+        <h3>Story</h3>
+        <p>{post.story}</p>
+        <div>
+          <button>
+            <LoveLink></LoveLink>
+          </button>
+        </div>
+      </section>
+    );
+  }
 }
