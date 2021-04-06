7eb6423d4802a566df37b9fa11bcc55b42a8f530
fersines <fsinesc@gmail.com>
2021-04-06 17:58:02 +0200

    Más portada CSS

--- FRONT/src/pages/Home.js
+++ FRONT/src/pages/Home.js
@@ -4,11 +4,11 @@ export default function Home() {
   return (
     <>
       <div className="portada">
-        <img src="/portada1.jpg" alt="Niño retransmitiendo" />
+        <img src="/portada1.jpg" alt="Niño gritando a un microfóno" />
         <div className="bienvenida">
           <h1>Bienvenido a LinkItUP!</h1>
-          <h2>El sito para compartir tus enlaces favoritos,</h2>
-          <h2>o no tan favoritos...</h2>
+          <h3>El sito para compartir tus enlaces favoritos,</h3>
+          <h3>o no tan favoritos...</h3>
         </div>
       </div>
     </>
